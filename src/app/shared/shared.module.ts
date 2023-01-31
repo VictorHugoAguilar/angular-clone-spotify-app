0d8b46897e8ea8aea1108e5e10af014dad6e3d4e
Victor Hugo Aguilar <victorhugoaguilar@aol.com>
2023-01-31 14:47:39 +0100

    Add files via upload

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,12 +4,20 @@ import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { MediaPlayerComponent } from './components/media-player/media-player.component';
 import { HeaderUserComponent } from './components/header-user/header-user.component';
 import { RouterModule } from '@angular/router';
+import { CardPlayerComponent } from './components/card-player/card-player.component';
+import { SectionGenericComponent } from './components/section-generic/section-generic.component';
+import { PlayListBodyComponent } from './components/play-list-body/play-list-body.component';
+import { PlayListHeaderComponent } from './components/play-list-header/play-list-header.component';
 
 @NgModule({
   declarations: [
     SideBarComponent,
     MediaPlayerComponent,
-    HeaderUserComponent
+    HeaderUserComponent,
+    CardPlayerComponent,
+    SectionGenericComponent,
+    PlayListBodyComponent,
+    PlayListHeaderComponent
   ],
   imports: [
     CommonModule,
@@ -18,7 +26,11 @@ import { RouterModule } from '@angular/router';
   exports: [
     SideBarComponent,
     MediaPlayerComponent,
-    HeaderUserComponent
+    HeaderUserComponent,
+    CardPlayerComponent,
+    SectionGenericComponent,
+    PlayListBodyComponent,
+    PlayListHeaderComponent
   ]
 })
 export class SharedModule { }
